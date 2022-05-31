b4f05374c64af7b7fe8ac993b9d86286516ca670
Pawel <pawel.tomczyk85@gmail.com>
2022-05-31 17:43:46 +0200

    TASK16 completed

--- src/main/java/com/company/Container.java
+++ src/main/java/com/company/Container.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Container<T> {
+public class Container<T> implements Collection<T> {
     List<T> list;
     public static int counter=0;
 
@@ -16,5 +14,70 @@ public class Container<T> {
     public void addToList(T ob){
         this.list.add(ob);
         counter++;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
     }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -9,24 +9,24 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//        APIData connector = new APIData();
-//        try {
-//            String data = connector.getCurrencyData();
-//            String[] array = data.split(",");
-//
-//            String numberValue;
-//            Double plnValue;
-//
-//            for (String s : array) {
-//                if (s.contains("PLN")) {
-//                    numberValue = s.split(":")[1];
-//                    plnValue = Double.parseDouble(numberValue);
-//                    System.out.println(plnValue);
-//                }
-//            }
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
+        APIData connector = new APIData();
+        try {
+            String data = connector.getCurrencyData();
+            String[] array = data.split(",");
+
+            String numberValue;
+            Double plnValue;
+
+            for (String s : array) {
+                if (s.contains("PLN")) {
+                    numberValue = s.split(":")[1];
+                    plnValue = Double.parseDouble(numberValue);
+                    System.out.println(plnValue);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
 
         //TASK 15
